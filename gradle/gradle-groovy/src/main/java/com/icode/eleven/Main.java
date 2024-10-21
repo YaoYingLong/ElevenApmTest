2b274be25dc80c73ce2912b457c701e5279eea2b
Y31201308 <906271196@qq.com>
2024-10-21 22:15:21 +0800

    feat: 添加Gradle模块

---
+++ gradle/gradle-groovy/src/main/java/com/icode/eleven/Main.java
@@ -0,0 +1,10 @@
+package com.icode.eleven;
+
+/**
+ * @author by YingLong on 2024/10/21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,8 @@ rootProject.name = "ElevenApmTest"
 
 include("netty:netty-4.1")
 include("lettuce:lettuce-5.1")
-include("gradle:gradle-common")
+include("gradle:gradle-kotlin")
+include("gradle:gradle-groovy")
 
 include("template:template-1.0")
+
